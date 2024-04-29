d267d5a50b8ec5df629e9e1df4660eb84ac4bb40
miautawn <miautawn@gmail.com>
2024-04-29 20:31:46 +0300

    Add global logger conf

--- src/avh/__init__.py
+++ src/avh/__init__.py
@@ -1,8 +1,24 @@
 import logging
+import tqdm
 
-# configure the root formatter
+# Configure Tqdm with default logging module
+# Taken from: https://stackoverflow.com/questions/38543506/change-logging-print-function-to-tqdm-write-so-logging-doesnt-interfere-wit/38739634#38739634
+class TqdmLoggingHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET):
+        super().__init__(level)
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.tqdm.write(msg)
+            self.flush()
+        except Exception:
+            self.handleError(record) 
+
+# Configure the root formatter
 logging.basicConfig(
-    format='{asctime}|{levelname}|{name}: {message}',
+    format='{asctime}|{levelname}|{module}.{funcName}: {message}',
     datefmt = "%Y-%m-%d %H:%M:%S",
-    style="{", level=logging.INFO
+    style="{", level=logging.INFO,
+    handlers = [TqdmLoggingHandler()]
     )
